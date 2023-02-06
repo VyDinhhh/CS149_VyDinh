aa0898377932f7bbc10fa142e9f336a1158fd85a
VyDinhhh <ivydinhh@gmail.com>
2023-02-05 20:16:02 -0800

    Resubmit- Class 1 hands on worksheet

--- lab1_18.c
+++ lab1_18.c
@@ -1,3 +1,11 @@
+/**
+* Description: Program print out three line of strings
+* Author name: Vy Dinh
+* Author email: thithuyvy.dinh@sjsu.edu
+* Last modified date: 01/30/2023
+* Creation date: 02/2/2023
+**/
+
 #include <stdio.h>
 
 int main(void) {
